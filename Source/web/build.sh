2d6319a4e31df33d3f837f8623f6f1a4e8152084
Rasmus Pedersen <rasmus@akvaservice.dk>
2012-02-17 16:17:54 +0100

    -

---
+++ Source/web/build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Usage: build.sh [outputdirectory]
+
+####################################################
+# INIT                                             #
+####################################################
+BASEDIR=$(dirname "$1")
+OUTPUTDIR="$1"
+
+####################################################
+# CLEAN                                            #
+####################################################
+echo "Cleaning previous build..."
+rm "$OUTPUTDIR/cgi-bin/Addins/Google/data/" -r
+
+####################################################
+# CGI-BIN                                          #
+####################################################
+echo "Building 'cgi-bin'..."
+for file in cgi-bin/*; do
+    cp -rv $file "$OUTPUTDIR/cgi-bin/"
+done
+
+####################################################
+# SETUP                                            #
+####################################################
+echo "Setting up build structur..."
+mkdir "$OUTPUTDIR/cgi-bin/Addins/Google/data/html/js/"
+
+####################################################
+# JAVASCRIPT                                       #
+####################################################
+echo "Building 'javascript'..."
+#jsbuilder javascript.jsb "$OUTPUTDIR/cgi-bin/Addins/sForm/data/html/js/"
+
+
---
+++ Source/web/javascript.jsb
@@ -0,0 +1,16 @@
+<solution name="sCMS" outputdirectory="">	
+	<project name="scms">
+		<class name="sCMS">	
+			<class name="root">
+				<js file="javascript/root.js" />
+			</class>		
+			<class name="page">
+				<js file="javascript/page.js" />
+			</class>
+			<class name="stylesheet">
+				<js file="javascript/stylesheet.js" />
+			</class>			
+		</class>
+	</project>	
+</solution>
+
